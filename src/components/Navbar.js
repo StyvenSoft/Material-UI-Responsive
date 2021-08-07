a20878c3e05b8f9625cdad3477982456b3f574b0
Steveen <steveen.echeverri@gmail.com>
2021-08-07 00:23:11 -0500

    add butto materia-iu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
-import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { alpha, AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: "flex",
+    justifyContent: 'space-between'
+  },
   logoLg: {
     display: 'none',
     [theme.breakpoints.up('sm')]:{
@@ -13,6 +19,15 @@ const useStyles = makeStyles((theme) => ({
       display: 'none',
     }
   },
+  search: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+  borderRadius: theme.shape.borderRadius,
+  },
 }));
 
 const Navbar = () => {
@@ -21,13 +36,17 @@ const Navbar = () => {
 
   return (
     <AppBar>
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
           <Typography variant="h6" className={classes.logoLg}>
             News Technology
           </Typography>
           <Typography variant="h6" className={classes.logoSm}>
             News
           </Typography>
+          <div className={classes.search}>
+            <Search />
+            <InputBase placeholder="Search..." />
+          </div>
         </Toolbar>
     </AppBar>  
   );
