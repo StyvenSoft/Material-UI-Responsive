f123f4b0522e2a3d24c197c7517b7a5a1d87d762
Steveen <steveen.echeverri@gmail.com>
2021-08-08 13:24:53 -0500

    Completed navBar add incons ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import { alpha, AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
-import { Search } from '@material-ui/icons';
+import { alpha, AppBar, Badge, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { Mail, Notifications, Search } from '@material-ui/icons';
 import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,7 +26,24 @@ const useStyles = makeStyles((theme) => ({
     '&:hover': {
       backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
-  borderRadius: theme.shape.borderRadius,
+    borderRadius: theme.shape.borderRadius,
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  input: {
+    color: "white",
+    marginLeft: theme.spacing(1),
+  },
+  searchButton: {
+    marginRight: theme.spacing(2),
+  },
+  icons: {
+    display: "flex",
+    alingItems: "center",
+  },
+  badge: {
+    marginRight: theme.spacing(2),
   },
 }));
 
@@ -45,7 +62,16 @@ const Navbar = () => {
           </Typography>
           <div className={classes.search}>
             <Search />
-            <InputBase placeholder="Search..." />
+            <InputBase className={classes.input} placeholder="Search..." />
+          </div>
+          <div className={classes.icons}>
+            <Search className={classes.searchButton} />
+            <Badge className={classes.badge} badgeContent={4} color="secondary">
+              <Mail />
+            </Badge>
+            <Badge className={classes.badge} badgeContent={4} color="secondary">
+              <Notifications />
+            </Badge>
           </div>
         </Toolbar>
     </AppBar>  
