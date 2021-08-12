21bf8aab97fd5692db934c6bdf701eecabd70138
Steveen <steveen.echeverri@gmail.com>
2021-08-11 22:59:58 -0500

    Created notification snackbar

--- src/components/Add.js
+++ src/components/Add.js
@@ -4,6 +4,12 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { Button, Container, Fab, FormLabel, makeStyles, MenuItem, Modal, TextField, Tooltip } from '@material-ui/core';
 import { Add as AddIcon } from '@material-ui/icons';
 import { useState } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme) => ({
     fab: {
@@ -32,7 +38,15 @@ const useStyles = makeStyles((theme) => ({
 
 const Add = () => {
     const classes = useStyles();
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [openAlert, setOpenAlert] = useState(false);
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenAlert(false);
+    };
 
     return (
         <>
@@ -76,16 +90,35 @@ const Add = () => {
                                 <FormControlLabel value="Everybody" control={<Radio size="small" />} label="Everybody" />
                                 <FormControlLabel value="My friends" control={<Radio size="small" />} label="My friends" />
                                 <FormControlLabel value="NoBody" control={<Radio size="small" />} label="NoBody" />
-                                <FormControlLabel value="Custom" disabled control={<Radio size="small"/>} label="(Premium)" />
+                                <FormControlLabel value="Custom" disabled control={<Radio size="small" />} label="(Premium)" />
                             </RadioGroup>
                         </div>
                         <div className={classes.item}>
-                            <Button variant="outlined" color="primary" style={{ marginRight: 20 }}>Create</Button>
-                            <Button variant="outlined" color="secondary" onClick={() => setOpen(false)}>Cancel</Button>
+                            <Button 
+                                variant="outlined" 
+                                color="primary" 
+                                style={{ marginRight: 20 }}
+                                onClick={() => setOpenAlert(true)}
+                            >Create</Button>
+                            <Button 
+                                variant="outlined" 
+                                color="secondary" 
+                                onClick={() => setOpen(false)}
+                            >Cancel</Button>
                         </div>
                     </form>
                 </Container>
             </Modal>
+            <Snackbar 
+                open={openAlert} 
+                autoHideDuration={6000} 
+                onClose={handleClose}
+                anchorOrigin={{ vertical: "bottom", horizontal: "left"}}
+            >
+                <Alert onClose={handleClose} severity="success">
+                    This is a test creation message!
+                </Alert>
+            </Snackbar>
         </>
     );
 };
