1cc79ee10a141d67bd4031a324f34e49d79e37d4
Steveen <steveen.echeverri@gmail.com>
2021-08-13 20:55:44 -0500

    Created my Avatar and online friends rightbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,13 @@
-import { alpha, AppBar, Badge, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { 
+    alpha, 
+    AppBar, 
+    Avatar, 
+    Badge, 
+    makeStyles, 
+    Toolbar, 
+    Typography, 
+    withStyles 
+} from '@material-ui/core';
 import { Cancel, Mail, Notifications, Search } from '@material-ui/icons';
 import InputBase from '@material-ui/core/InputBase';
 import React, { useState } from 'react';
@@ -10,10 +19,10 @@ const useStyles = makeStyles((theme) => ({
   },
   logoLg: {
     display: 'none',
-    [theme.breakpoints.up('sm')]:{
+    [theme.breakpoints.up('sm')]: {
       display: 'block',
     }
-  }, 
+  },
   logoSm: {
     display: 'block',
     [theme.breakpoints.up('sm')]: {
@@ -53,8 +62,38 @@ const useStyles = makeStyles((theme) => ({
   },
   badge: {
     marginRight: theme.spacing(2),
+    marginTop: theme.spacing(1),
   },
 }));
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
 
 const Navbar = () => {
 
@@ -63,32 +102,42 @@ const Navbar = () => {
 
   return (
     <AppBar position="fixed">
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" className={classes.logoLg}>
-            News Technology
-          </Typography>
-          <Typography variant="h6" className={classes.logoSm}>
-            News
-          </Typography>
-          <div className={classes.search}>
-            <Search />
-            <InputBase className={classes.input} placeholder="Search..." />
-            <Cancel className={classes.cancel} onClick={() => setOpen(false)} />
-          </div>
-          <div className={classes.icons}>
-            <Search 
-              className={classes.searchButton}
-              onClick={() => setOpen(true)}
-            />
-            <Badge className={classes.badge} badgeContent={4} color="secondary">
-              <Mail />
-            </Badge>
-            <Badge className={classes.badge} badgeContent={4} color="secondary">
-              <Notifications />
-            </Badge>
-          </div>
-        </Toolbar>
-    </AppBar>  
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.logoLg}>
+          News Technology
+        </Typography>
+        <Typography variant="h6" className={classes.logoSm}>
+          News
+        </Typography>
+        <div className={classes.search}>
+          <Search />
+          <InputBase className={classes.input} placeholder="Search..." />
+          <Cancel className={classes.cancel} onClick={() => setOpen(false)} />
+        </div>
+        <div className={classes.icons}>
+          <Search
+            className={classes.searchButton}
+            onClick={() => setOpen(true)}
+          />
+          <Badge className={classes.badge} badgeContent={4} color="secondary">
+            <Mail />
+          </Badge>
+          <Badge className={classes.badge} badgeContent={4} color="secondary">
+            <Notifications />
+          </Badge>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            variant="dot"
+          >
+            <Avatar alt="Steveen Echeverry" src="https://avatars.githubusercontent.com/u/10743862" />
+          </StyledBadge>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,4 +1,10 @@
-import { Container, makeStyles } from '@material-ui/core';
+import { 
+        Avatar, 
+        Container, 
+        makeStyles, 
+        Typography 
+} from '@material-ui/core';
+import { AvatarGroup } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -8,7 +14,16 @@ const useStyles = makeStyles((theme) => ({
 
 const Rightbar = () => {
     const classes = useStyles();
-    return <Container className={classes.container}>rightbar</Container>;
+    return <Container className={classes.container}>
+        <Typography>Online Friends</Typography>
+        <AvatarGroup max={4}>
+            <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+            <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/2.jpg" />
+            <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/3.jpg" />
+            <Avatar alt="Agnes Walker" src="https://material-ui.com/static/images/avatar/4.jpg" />
+            <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/5.jpg" />
+        </AvatarGroup>
+    </Container>;
 };
 
 export default Rightbar;
